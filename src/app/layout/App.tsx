bdffc195c1775e38ad6cb3b82582d8dd9ab118ab
fepu08 <ferencbesenyei@protonmail.com>
2021-04-01 14:15:27 +0200

    set up basic react routing and created log in layout

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
+import LoginForm from "../../features/users/LoginForm";
+import RegisterForm from "../../features/users/RegisterForm";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <Router>
+      {/* place navbar here*/}
+      <Container fluid>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/register" component={RegisterForm} />
+          <Route exact path="/login" component={LoginForm} />
+        </Switch>
+      </Container>
+    </Router>
   );
 }
 
--- src/features/users/LoginForm.tsx
+++ src/features/users/LoginForm.tsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { Button, Form } from "react-bootstrap";
 
 const LoginForm = () => {
-  return <div></div>;
+  return (
+    <div
+      className="d-flex flex-column justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <h1 className="text-center mb- mb-5">
+        <span className="text-primary">L</span>og&nbsp;
+        <span className="text-primary">I</span>n
+      </h1>
+      <Form style={{ maxWidth: "500px" }} className="card w-100 p-3">
+        <div className="card-body">
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" required />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" required />
+          </Form.Group>
+          <Button
+            className="d-block mt-4 ml-auto mr-auto w-100 "
+            variant="primary"
+            type="submit"
+          >
+            Submit
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
 };
 
 export default LoginForm;
