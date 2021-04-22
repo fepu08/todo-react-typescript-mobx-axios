f9dbd42e9ad22c5ab63df7af8f2d07ec07fbc5d9
fepu08 <ferencbesenyei@protonmail.com>
2021-04-22 11:49:12 +0200

    basic todo componets to check request and stores

--- src/features/todos/dashboard/TodoDashboard.tsx
+++ src/features/todos/dashboard/TodoDashboard.tsx
@@ -1,9 +1,18 @@
 // list todos
 // add todo button
 import React from "react";
+import { Button } from "react-bootstrap";
+import TodoList from "./TodoList";
 
 const TodoDashboard = () => {
-  return <div>Todos</div>;
+  return (
+    <>
+      <div className="d-ddddddflex justify-content-between my-3">
+        <h1>Todos</h1>
+      </div>
+      <TodoList />
+    </>
+  );
 };
 
 export default TodoDashboard;
--- src/features/todos/dashboard/TodoList.tsx
+++ src/features/todos/dashboard/TodoList.tsx
@@ -1,7 +1,20 @@
+import { observer } from "mobx-react-lite";
 import React from "react";
+import { useStore } from "../../../app/stores/store";
 
 const TodoList = () => {
-  return <div></div>;
+  const {
+    todoStore: { todos },
+  } = useStore();
+
+  if (todos === []) return <h3>Please create a TODO first</h3>;
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li>{todo.title}</li>
+      ))}
+    </ul>
+  );
 };
 
-export default TodoList;
+export default observer(TodoList);
